cbcbe08902bc0104ad9d05ff9b8752b8c9581614
Jeremy Rue <jrue@users.noreply.github.com>
2020-01-28 09:46:30 -0800

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+
+
+    //watch for changes, and do a different action depending on situation
+    watch: {
+      options: {
+        spawn: false,
+        event: ['all'],
+        livereload: true,
+      },
+      scripts: {
+        files: '**/*.js'
+      },
+      styles: {
+        files: '**/*.css'
+      },
+      html: {
+        files: ['**/*.html']
+      }
+    },
+
+    //make a webserver on the dist folder
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: 'localhost',
+          livereload: true,
+          // keepalive: true,
+          base: "./src"
+        }
+      }
+    }
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  //create build folder and run watch task
+  grunt.registerTask('default', ['connect','watch']);
+
+};
